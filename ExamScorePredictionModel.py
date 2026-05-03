10e1855479e1ec9f53dbe771772b4478d60ed254
Omari <infonet20th@gmail.com>
2026-05-03 11:19:03 +0300

    Comm

--- ExamScorePredictionModel.py
+++ ExamScorePredictionModel.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import numpy as np
 import pickle
 
 # pickle.dump(gModel,open("gModel.pkl","wb"))
@@ -14,20 +15,20 @@ tab1,tab2=st.tabs(['User Input','Upload File'])
 
 
 with tab1:
-    name=st.text_input("Enter your name")
+    Methodology=st.text_input("Enter Methodology  Eg Offline, Online, Hybrid",placeholder="Offline, Online, Hybrid")
     Age=st.number_input("Enter Your Age")
     Hours=st.number_input("Enter Study Hours")
     button=st.button("predict")
 
     if button:
-        if name.isnumeric():
-            st.error("Enter real name")
+        if Methodology.isnumeric():
+            st.error("Enter Methodology Eg Offline, Online, Hybrid")
         elif Age<=0:
               st.error("Enter Real Age")
         elif Hours<=0:
                st.error("Enter Real Hours")
         else:
-            df=pd.DataFrame({"Name":[name],"Age":[Age],"Study Hours":[Hours]})
+            df=pd.DataFrame({"Methodology":[Methodology],"Age":[Age],"Study Hours":[Hours]})
             transform=combined.transform(df)
             predict=gModel.predict(transform)
             actualScore=np.expm1(predict)
